5f76e5dc85369261aca19c0df8e09c7dc762c9cf
Shashank B C <bcshashank140@gmail.com>
2024-10-23 22:03:35 +0545

    Update hello.js

--- Masonry Style Layout/hello.js
+++ Masonry Style Layout/hello.js
@@ -1,1 +1,13 @@
-console.log("hello react");
+let students = ['John', 'Sara', 'Jack'];
+
+// using forEach
+students.forEach(myFunction);
+
+function myFunction(item) {
+
+    console.log(item);
+}
+
+let array= ["parachute", "boys like girls", "neck ddep"];
+
+array.forEach( item=>console.log(item) );
